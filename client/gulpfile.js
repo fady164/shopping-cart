a6dcda54e1933c9b7f8796ca9c4d0da7ff44c208
fady164 <76261718+fady164@users.noreply.github.com>
2022-12-05 17:24:34 +0200

    start react project

---
+++ client/gulpfile.js
@@ -0,0 +1,14 @@
+//top level function [task - src - dest - watch]
+
+// task ==> do task or function
+// src ==> 22dr ashof file mo3in
+// dest ( destination) ==> ybos mn al src w yohoto 3n tari2 al dest
+// watch ==> ay taghiir yshofo dynamic
+
+const gulp = require("gulp");
+const gulpSass = require("gulp-sass");
+const sass = gulpSass(require("sass"));
+
+gulp.task("sass", async () => {
+   gulp.src("src/*.scss").pipe(sass()).pipe(gulp.dest("src/css"));
+});
