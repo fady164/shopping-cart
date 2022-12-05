80ad2182d839d3234e75c60adb66917f6a4f46b9
fady164 <76261718+fady164@users.noreply.github.com>
2022-12-05 17:50:27 +0200

    header-comp

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -10,5 +10,5 @@ const gulpSass = require("gulp-sass");
 const sass = gulpSass(require("sass"));
 
 gulp.task("sass", async () => {
-   gulp.src("src/*.scss").pipe(sass()).pipe(gulp.dest("src/css"));
+   gulp.src("src/components/**/*.scss").pipe(sass()).pipe(gulp.dest("src/css"));
 });
--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
-   return <div className="App">App</div>;
+   return (
+      <div className="layout">
+         <Header />
+         <main>content</main>
+         <footer>footer</footer>
+      </div>
+   );
 }
 
 export default App;
