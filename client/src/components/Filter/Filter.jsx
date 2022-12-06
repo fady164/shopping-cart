c91b0da35f64728aea01b1ff5ac06372e052757d
fady164 <76261718+fady164@users.noreply.github.com>
2022-12-06 16:29:18 +0200

    filter-comp

--- client/src/components/Filter/Filter.jsx
+++ client/src/components/Filter/Filter.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import "../../css/Filter/filter.css";
 
-export default function Filter() {
+export default function Filter({
+   handleFilterBySize,
+   size,
+   handleFilterByOrder,
+   sort,
+}) {
    return (
       <div className="filter-wrapper">
          <h2 className="filter-title">Filter</h2>
          <div className="num-of-products">Number of product 4</div>
          <div className="filter-by-size">
             <span>Filter</span>
-            <select className="filter-select ">
+            <select
+               value={size}
+               className="filter-select"
+               onChange={handleFilterBySize}
+            >
                <option value="all">All</option>
                <option value="s">S</option>
                <option value="m">M</option>
@@ -18,9 +27,13 @@ export default function Filter() {
          </div>
          <div className="filter-by-order">
             <span>Order</span>
-            <select className="filter-select ">
+            <select
+               value={sort}
+               className="filter-select"
+               onChange={handleFilterByOrder}
+            >
                <option value="latest">Latest</option>
-               <option value="lower">Lower</option>
+               <option value="lowest">Lowest</option>
                <option value="highest">Highest</option>
             </select>
          </div>
